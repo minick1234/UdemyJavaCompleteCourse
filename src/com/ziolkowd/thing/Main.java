eb34ee71a916d469a652cd122ed839220254cd0b
minick1234 <minickisbeast@gmail.com>
2020-05-12 19:01:20 -0400

    no message

--- src/com/ziolkowd/thing/Main.java
+++ src/com/ziolkowd/thing/Main.java
@@ -24,6 +24,7 @@ public class Main {
         //These 2 things basically do the opposite number because back to math in first sem , the overflow of a number changes it to the opposite of the value
         System.out.println("This is a example of something called a overflow for the max value : " + (myValue1 + 1));
         System.out.println("This is a example of something called a overflow for the min value : " + (myValue2 - 1));
+        System.out.println("-----------------------------------------------------------------------");
 
         //Uncommented this returns a error because you cant set it out of the length possible, so this shows how when during the sout above it auto inverts the numbers but if i try to pass it in general it wont work!
         //int maxIntTest = 2147483648;
@@ -35,14 +36,23 @@ public class Main {
 
         byte myMaxByteValue = Byte.MAX_VALUE;
         byte myMinByteValue = Byte.MIN_VALUE;
-
         System.out.println("My Max byte value: " + myMaxByteValue);
         System.out.println("My Min byte value: " + myMinByteValue);
         System.out.println("The max value of byte is :" + myMaxByteValue + " my min value of byte is :" + myMinByteValue);
+        System.out.println("-----------------------------------------------------------------------");
 
-         System.out.println("This is a example of something called a overflow for the max value : " + (myMaxByteValue + 1)); // This doesnt exist for bytes because after this is becomes a int!
-        System.out.println("This is a example of something called a overflow for the min value : " + (myMinByteValue - 1)); // This doesnt exist for bytes because after this is becomes a int!
+        short myMaxShortValue = Short.MAX_VALUE;
+        short myMinShortValue = Short.MIN_VALUE;
+        System.out.println("My Max Short value: " + myMaxShortValue);
+        System.out.println("My Min Short value: " + myMinShortValue);
+        System.out.println("The max value of Short is :" + myMaxShortValue + " my min value of Short is :" + myMinShortValue);
+        System.out.println("-----------------------------------------------------------------------");
 
+        long myMaxLongValue = Long.MAX_VALUE;
+        long myMinLongValue = Long.MIN_VALUE;
+        System.out.println("My Max Short value: " + myMaxLongValue);
+        System.out.println("My Min Short value: " + myMinLongValue);
+        System.out.println("The max value of Short is :" + myMaxLongValue + " my min value of Short is :" + myMinLongValue);
 
     }
 }
